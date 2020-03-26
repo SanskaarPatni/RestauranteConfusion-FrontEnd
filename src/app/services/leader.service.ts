e8ad17492c7262a10e2274296e35decbfe3d8f9b
Sanskaar Patni <patnisanskaar@gmail.com>
2020-03-27 01:09:33 +0530

    Added server side for leaders and promotions

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,23 +3,30 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leaders');
   }
   //filter out the first one which satisfies the condition
   getLeader(id: string): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.id == id)[0]).pipe(delay(2000));
+    return this.http.get<Leader>(baseURL + 'leaders/' + id);
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    return this.http.get<Leader>(baseURL + 'leaders?featured=true')
+      .pipe(map(leaders => leaders[0]));
+  }
 
+  getLeaderIds(): Observable<string[] | any> {
+    return this.getLeaders().pipe(map(leaders => leaders.map(leader => leader.id)));
   }
 }
